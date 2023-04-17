9b820702941d5713e017a51b317bf66997b7e1b9
GSUwebpros <120506454+GSUwebpros@users.noreply.github.com>
2023-04-17 09:50:57 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from auth import auth
-from views import views
-from db_connection import app
-
-app.register_blueprint(auth)
-app.register_blueprint(views)
-
-if __name__ == '__main__':
-    app.run(debug=True)
